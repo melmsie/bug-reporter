d530c8c7e73ceecffe986495c467e576dd29d66f
Melmsie <memyselfandlie@gmail.com>
2022-06-29 12:56:12 -0400

    cool

--- scripts/deploy-commands.js
+++ scripts/deploy-commands.js
@@ -4,17 +4,14 @@ const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('../config.json');
 
 const commands = [
-  new SlashCommandBuilder().setName('findsimilar').setDescription('Find similar bug reports so you do not annoy Kable')
-    .addStringOption(option => option.setName('description').setDescription('Describe your bug in a one liner').setRequired(true)),
+  new SlashCommandBuilder().setName('stats').setDescription('stats!')
+    .addUserOption(option => option.setName('user').setDescription('user for stats').setRequired(true)),
 
   new SlashCommandBuilder().setName('bug').setDescription('Post a bug in the bugs channel')
     .addStringOption(option => option.setName('description').setDescription('Describe the bug in detail').setRequired(true))
     .addStringOption(option => option.setName('reproduction').setDescription('Describe how we can reproduce it').setRequired(true))
     .addStringOption(option => option.setName('link').setDescription('Message link to the bug happening in this server').setRequired(false))
     .addAttachmentOption(option => option.setName('image').setDescription('A screenshot of the bug').setRequired(false))
-
-  // new SlashCommandBuilder().setName('feedback').setDescription('Post a bug in the bugs channel')
-  //   .addStringOption(option => option.setName('description').setDescription('Describe the bug in detail').setRequired(true)),
 ]
   .map(command => command.toJSON());
 
---
+++ src/commands/stats.js
@@ -0,0 +1,32 @@
+const config = require('../../config.json');
+const prisma = require('../utils/prisma');
+const functions = new (require('../utils/functions'))();
+const { PostType } = require('@prisma/client');
+const { Embed } = require('@discordjs/builders');
+module.exports = {
+  async run (interaction, client) {
+    const postData = await prisma.post.findMany({
+      where: {
+        userID: interaction.options.data[0]?.value,
+      },
+      include: {
+        votes: true
+      }
+    });
+
+    // I know this is bad code if you're reading this, I'm tired and slightly high and will fix it later
+
+    await interaction.reply({
+      embeds: [
+        {
+          description: `**Stats for <@${interaction.options.data[0]?.value}>**\n\n` +
+            `**\`Reported:\`** ${postData.length}\n` +
+            `**\`Valid & Fixed:\`** ${postData.filter(x=>x.solved === true).length} (${(postData.filter(x=>x.solved === true).length / postData.length) * 100}%)\n` +
+            `**\`Invalid & Removed:\`** ${postData.filter(x=>x.solved !== true && x.removed === true).length} (${(postData.filter(x=>x.solved !== true && x.removed === true).length / postData.length) * 100}%)\n` +
+            `**\`Pending:\`** ${postData.filter(x=>x.removed !== true).length}`
+        }
+      ],
+      ephemeral: true
+    });
+  }
+};
