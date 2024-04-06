bda62050b48385b712492ffca61fd14e5bf84fe0
Melmsie <memyselfandlie@gmail.com>
2024-04-05 21:39:31 -0400

    haha

--- src/handlers/button/admin.js
+++ src/handlers/button/admin.js
@@ -9,6 +9,7 @@ const admins = [
   '613065902723694623', // Zomb
   '304649407444287488', // DR. A
   '863986381738475531', // Angel
+];
 
 module.exports = {
   async handle (interaction, client) {
@@ -205,3 +206,18 @@ module.exports = {
     await interaction.deferUpdate();
   }
 };
+
+// 9|betareport  | /home/beta/betabugs/bug-reporter/src/handlers/button/admin.js:207
+// 9|betareport  | };
+// 9|betareport  |  ^
+// 9|betareport  | SyntaxError: Unexpected token ';'
+// 9|betareport  |     at Object.compileFunction (node:vm:352:18)
+// 9|betareport  |     at wrapSafe (node:internal/modules/cjs/loader:1032:15)
+// 9|betareport  |     at Module._compile (node:internal/modules/cjs/loader:1067:27)
+// 9|betareport  |     at Object.Module._extensions..js (node:internal/modules/cjs/loader:1155:10)
+// 9|betareport  |     at Module.load (node:internal/modules/cjs/loader:981:32)
+// 9|betareport  |     at Function.Module._load (node:internal/modules/cjs/loader:822:12)
+// 9|betareport  |     at Module.require (node:internal/modules/cjs/loader:1005:19)
+// 9|betareport  |     at Module.Hook._require.Module.require (/usr/lib/node_modules/pm2/node_modules/require-in-the-middle/index.js:80:39)
+// 9|betareport  |     at require (node:internal/modules/cjs/helpers:102:18)
+// 9|betareport  |     at Object.handle (/home/beta/betabugs/bug-reporter/src/handlers/button/index.js:9:7)
